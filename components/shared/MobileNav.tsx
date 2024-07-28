9af2618e73d3fca2fca0249e21fbf7d95e4eeda3
latic1 <laticlatif2@gmail.com>
2024-07-28 20:28:56 +0000

    neww

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -26,7 +26,7 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
           <Image 
-            src="/assets/images/logo.svg"
+            src="/assets/images/BSE-2-B@2x.png"
             alt="logo"
             width={128}
             height={38}
