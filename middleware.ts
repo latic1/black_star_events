e356c98609fc46497ac76a384ed73cd88b2688d9
latic1 <laticlatif2@gmail.com>
2024-07-29 13:18:57 +0000

    webhook check

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,34 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
  
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
-});
+// export default authMiddleware({
+//   publicRoutes: [
+//     '/',
+//     '/events/:id',
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ],
+//   ignoredRoutes: [
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ]
+// });
  
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+ 
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
- 
 
