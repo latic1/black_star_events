0d7a4ff1859fd93511388093371d45feb0dce22d
latic1 <laticlatif2@gmail.com>
2024-08-13 11:52:39 +0000

    call back

--- app/api/paystackCallback/route.ts
+++ app/api/paystackCallback/route.ts
@@ -18,7 +18,7 @@ export async function GET(request: NextRequest) {
         const response = await fetch(`https://api.paystack.co/transaction/verify/${reference}`, {
             method: 'GET',
             headers: {
-                Authorization: `Authorization: Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+                Authorization: `Bearer sk_test_52e71345d65215548fb96f84cea1ba2ed7e754d3`,
                 'Content-Type': 'application/json',
             },
         });
