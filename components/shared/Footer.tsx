a01ffe9dca0ed0c6cdd673fff9b355aff7c58b11
laticlatif <76080207+latic1@users.noreply.github.com>
2024-10-12 11:21:35 +0000

    Update Footer.tsx

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 Black Star. All Rights reserved.</p>
+        <p>2024 Black Star. All Rights reserved.</p>
       </div>
     </footer>
   );
